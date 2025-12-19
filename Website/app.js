7f0ee454bff28a21f20a8ba63755521ec00315e5
Nidonocu <nidonocu@gmail.com>
2025-12-19 00:24:17 +0000

    Release version of automatic Mochie shader package installer and automatic updater.

--- Website/app.js
+++ Website/app.js
@@ -1,10 +1,19 @@
-(async function main () {
+(async function main() {
     const response = await fetch('README.md');
 
     if (response.ok) {
         const readme = await response.text();
         var converter = new showdown.Converter(),
-        html = converter.makeHtml(readme);
+            html = converter.makeHtml(readme);
         document.querySelector('#readme-content').innerHTML = html;
+    }
+
+    const imgElement = document.getElementById("footer-logo");
+
+    const fallbackURL = "nidonocu_vr_logo.png";
+
+    imgElement.onerror = function () {
+        imgElement.onerror = null;
+        imgElement.src = fallbackURL;
     }
 })();
